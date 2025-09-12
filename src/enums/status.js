a64f44bf1b6d28850c2de2b4cf2e209146d8351d
Jackob2004 <zjakub2004@gmail.com>
2025-09-12 13:09:13 +0200

    Add statu module
    Define enum like type.
    Will serve as a part of task type.

---
+++ src/enums/status.js
@@ -0,0 +1,12 @@
+/**
+ *
+ * @readonly
+ * @enum {string}
+ */
+const Status = Object.freeze({
+    NOT_STARTED: 'not_started',
+    IN_PROGRESS: 'in_progress',
+    DONE: 'done',
+});
+
+export {Status};
