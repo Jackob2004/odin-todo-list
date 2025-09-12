9a0b3a1e31d400fc2139c707718fcfcd03a98785
Jackob2004 <zjakub2004@gmail.com>
2025-09-12 12:48:55 +0200

    Add prority module
    Define enum like type.
    Will serve as a part of task data object.

---
+++ src/enums/priority.js
@@ -0,0 +1,13 @@
+/**
+ *
+ * @readonly
+ * @enum {string}
+ */
+const Priority = Object.freeze({
+    LOW: 'low',
+    MEDIUM: 'medium',
+    HIGH: 'high',
+    URGENT: 'urgent'
+});
+
+export {Priority};
