b612c106d1319f73ec460b1f2a85733a39b6ebcd
Jackob2004 <zjakub2004@gmail.com>
2025-09-12 17:10:11 +0200

    Implement project module
    Provides functionality for creating project data objects.
    
    Choose map for storing tasks and notes to achieve fast lookups.
    There is no need for ordered data structure, since data will be sorted anyway.

---
+++ src/model/project.js
@@ -0,0 +1,26 @@
+/**
+ * @module project
+ */
+
+/**
+ * @typedef module:project.Project
+ * @type {Object}
+ * @property {string} title
+ * @property {Map<String, module:task.Task>} tasks task id as a key and task obj as a value
+ * @property {Map<String, module:note.Note>} notes note id as a key and note obj as a value
+ * @property {string} id A string containing a randomly generated, 36 character long v4 UUID
+ */
+
+/**
+ * @param {string} title
+ * @returns {module:project.Project}
+ */
+function createProject(title) {
+    const id = crypto.randomUUID();
+    const tasks = new Map();
+    const notes = new Map();
+
+    return {title, tasks, notes, id};
+}
+
+export {createProject};
