76698da958158bba5fbee958c4fb32199fee716e
Jackob2004 <zjakub2004@gmail.com>
2025-09-16 11:38:00 +0200

    Implement opening projects
    Create generic function for displaying cards to avoid code duplication.
    Implement opening projects and switching back to all projects.

--- src/main-ui-controller.js
+++ src/main-ui-controller.js
@@ -1,11 +1,41 @@
-import {Status} from "./enums/status";
+import {Status} from "./enums/status.js";
+import {selectProject, getAllTasks, getAllProjects, getProjectName} from "./project-service.js";
 
 /**
  * @module mainUIController responsible for UI interaction in the main section of the page.
- * Enables viewing projects and their contents
+ * Enables viewing projects and their contents.
  */
 
 const projectsContainer = document.querySelector("#projects-container");
+const projectsInfoDisplay = document.querySelector("#projects-info h2");
+const backButton = document.querySelector("#btn-back");
+
+projectsContainer.addEventListener("click", (event) => {
+   if (event.target.dataset.projectId) {
+       openProject(event.target.dataset.projectId);
+   }
+});
+
+// come back to all projects
+backButton.addEventListener("click", () => {
+    displayProjects(getAllProjects());
+    projectsInfoDisplay.textContent = "All Projects";
+    backButton.disabled = true;
+});
+
+/**
+ *
+ * @param {string} projectId
+ */
+function openProject(projectId) {
+    selectProject(projectId);
+    const tasks = getAllTasks();
+    if (!tasks) return;
+
+    displayTasks(tasks);
+    projectsInfoDisplay.textContent = getProjectName();
+    backButton.disabled = false;
+}
 
 /**
  * @param {module:projectService.ProjectSummary} projectData
@@ -34,19 +64,6 @@ function generateProjectCard(projectData) {
 }
 
 /**
- * @param {Array<module:projectService.ProjectSummary>} projects
- */
-function displayProjectsCards(projects) {
-    const fragment = document.createDocumentFragment();
-
-    for (const project of projects) {
-        fragment.appendChild(generateProjectCard(project));
-    }
-
-    projectsContainer.appendChild(fragment);
-}
-
-/**
  * @param {module:projectService.TaskSummary} taskSummary
  */
 function generateTaskCard(taskSummary) {
@@ -55,8 +72,6 @@ function generateTaskCard(taskSummary) {
     const selectStatus = document.createElement("select");
     const dateInfo = document.createElement("p");
     const deleteButton = document.createElement("button");
-
-    heading.textContent = taskSummary.title;
 
     for (const status of Object.values(Status)) {
         const option = document.createElement("option");
@@ -69,14 +84,45 @@ function generateTaskCard(taskSummary) {
         selectStatus.appendChild(option);
     }
 
+    heading.textContent = taskSummary.title;
     dateInfo.textContent = taskSummary.dueDate.toLocaleDateString();
     deleteButton.textContent = "X";
 
     card.dataset.taskId = taskSummary.id;
     card.setAttribute("class", "task-card");
+
     card.append(heading, selectStatus, dateInfo, deleteButton);
 
     return card;
 }
 
-export {displayProjectsCards};
+/**
+ * Displays cards in projects container
+ * @param {Array<any>} cardsData The data from which cards will be made of.
+ * @param {Function} generateCard A function that takes card data and returns a card element.
+ */
+function displayCards(cardsData, generateCard) {
+    const fragment = document.createDocumentFragment();
+
+    for (const item of cardsData) {
+        fragment.appendChild(generateCard(item));
+    }
+
+    projectsContainer.replaceChildren(fragment);
+}
+
+/**
+ * @param {Array<module:projectService.ProjectSummary>} summaries
+ */
+function displayProjects(summaries) {
+    displayCards(summaries, generateProjectCard);
+}
+
+/**
+ * @param {Array<module:projectService.TaskSummary>} summaries
+ */
+function displayTasks(summaries) {
+    displayCards(summaries, generateTaskCard);
+}
+
+export {displayProjects};
